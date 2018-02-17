400d4fc6b750f056bfa70f4e0f03d17f035763a0
failte <lsimon@student.42.fr>
2018-02-17 14:31:45 +0100

    Setup loaders for sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 var path = require('path');
 
 module.exports = {
-  entry: './src/client/index.js',
+    entry: './src/client/index.js',
 
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
 
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query:{
-        presets: ["es2015", "react", "stage-0"]
-      }
-    }]
-  }
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ["es2015", "react", "stage-0"]
+            }
+        }, {
+            test: /\.scss$/,
+            loaders: [
+                "style-loader", "css-loader", "sass-loader"
+            ]
+        }]
+    }
 };
