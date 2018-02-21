4c6dba758e19632dbfada4aadb11a44efe09f4cf
failte <lsimon@student.42.fr>
2018-02-21 06:23:57 +0100

    socketMiddleWare: refacto

--- src/client/middleware/socketIoMiddleware.js
+++ src/client/middleware/socketIoMiddleware.js
@@ -1,7 +1,5 @@
-const socketIoMiddleWare = socket => ({dispatch, getState}) => {
-	if(socket) socket.on('action', action => {
-		dispatch(action)
-	})
+const socketIoMiddleWare = socket => ({dispatch}) => {
+	if(socket) socket.on('action', dispatch)
 	return next => action => {
 		if(socket && action.type && action.type.indexOf('SERVER_') === 0)
 			// send this action to the server
