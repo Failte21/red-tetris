5c8632b15b6a57bb301625845e0c20bb5c5b3d74
Elizabeth Kelen <elizabethkelen@liz.local>
2018-03-04 14:50:07 +0100

    adds rotation unit tests

--- test/geometry.js
+++ test/geometry.js
@@ -7,73 +7,86 @@ const assert = chai.assert
 const expect = chai.expect
 const should = chai.should()
 
-const PIECES = TETROS.PIECES
-
-const examples = {
-    I: {
-        piece: [[0,1],[1,1],[2,1],[3,1]],
-        shape: [
-            [0,0,0,0],
-            [1,1,1,1],
-            [0,0,0,0],
-            [0,0,0,0],
-        ],
-        rotated: {
-            piece: [[2,0],[2,1],[2,2],[2,3]],
-            shape: [[0, 0, 1, 0],
-                    [0, 0, 1, 1],
-                    [0, 0, 1, 0],
-                    [0, 0, 1, 0]]
+describe('pieceToShape(piece)', () => {
+    it(`takes an array of coordinates and maps them to a 4x4 matrix`, done => {
+        for (let piece of TETROS.PIECES) {
+            const shape = pieceToShape(piece)
+            expect(shape).to.have.lengthOf(4)
+            assert(shape.every(row => row.length === 4), `Not every row in ${piece} has length of 4`)
         }
-    },
-    J: {
-        piece: [[0,1],[1,1],[2,1],[0,0]],
-        shape: [
-            [ 1, 0, 0, 0 ],
-            [ 1, 1, 1, 0 ],
-            [ 0, 0, 0, 0 ],
-            [ 0, 0, 0, 0 ] ],
-        rotated: {
-            piece: {},
-            shape: [ [ 0, 0, 1, 1 ], [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 0, 0 ] ]
-        }
-    }
-}
-
-const logShape = (shape) => {
-    shape.forEach((row) => {
-        console.log(row)
+        done()
     })
-}
+    it('each matrix is filled with 0s, except for four 1s', done => {
+        for (let piece of TETROS.PIECES) {
+            const shape = pieceToShape(piece)
+            assert(_.flatten(shape).filter(val => val === 1).length === 4, 'there are more than four 1s in shape')
+            assert(_.flatten(shape).filter(val => val === 0).length === 12, 'incorrect empty spaces in shape')
+        }
+        done()
+    })
+    it('correctly maps each coordinate from "piece" to a "1" on the matrix', done => {
+        for (let piece of TETROS.PIECES) {
+            const shape = pieceToShape(piece)
+            assert(piece.every(pt => shape[pt[1], pt[0]]), `some coordinates are not mapped as 1s for ${piece}`)
+        }
+        done()
+    })
+})
 
-//console.log(pieceToShape(examples.J.piece))
-console.log(shapeToPiece(rotate(examples.J.shape)))
+describe('shapeToPiece(piece)', () => {
+    it(`turns shape back into original piece if used with pieceToShape`, done => {
+        for (let piece of TETROS.PIECES) {
+            const shape = pieceToShape(piece)
+            const returnedPiece = shapeToPiece(shape)
+            expect(returnedPiece).to.have.all.deep.members(piece, `original ${piece} does not equal return: ${returnedPiece}`)
+        }
+        done()
+    })
+})
 
-// describe('pieceToShape(piece)', () => {
-//     for (let eg in examples) {
-//         it(`returns only visible rows of a ${examples[eg].desc}`, done => {
-//             expect(getVisibleBoard(examples[eg].data)).to.have.lengthOf(examples[eg].data.length - 2)
-//             expect(_.last(getVisibleBoard(examples[eg].data))).to.eql(_.last(examples[eg].data))
-//             expect(getVisibleBoard(examples[eg].data)[0]).to.eql(examples[eg].data[2])
-//             done()
-//         })
-//     }
-// })
-//
-// describe('getSpectreFromMatrix(visibleBoardData', () => {
-//     let spectre
-//     let visibleEmptyBoard = getVisibleBoard(examples.validEmptyBoard.data)
-//     let visibleInProgressBoard = getVisibleBoard(examples.validBoardInProgress.data)
-//     it (`if given matrix of 0s, returns a string of 'null' that is same length as matrix width.`, () => {
-//         spectre = getSpectreFromMatrix(visibleEmptyBoard)
-//         expect(spectre).to.have.lengthOf(visibleEmptyBoard[0].length)
-//         assert(spectre.every(val => _.isNull(val)), 'not every val is null')
-//     })
-//     it(`if given matrix which contains no values > 0 in first col, will return array with null in first column only.`, () => {
-//         spectre = getSpectreFromMatrix(visibleInProgressBoard)
-//         assert(_.isNull(spectre[0]) && spectre.slice(1).every(val => !_.isNull(val)), 'not an array with only first value as null')
-//     })
-// })
+describe('rotate(shape)', () => {
+    it(`returns a 4x4 of ints (shape) from a 4x4 of ints`, done => {
+        for (let piece of TETROS.PIECES) {
+            const shape = pieceToShape(piece)
+            const rotatedShape90 = rotate(shape)
+            const rotatedShape180 = rotate(rotatedShape90)
+            const rotatedShape270 = rotate(rotatedShape180)
+
+            expect(rotatedShape90 && rotatedShape180 && rotatedShape270).to.have.lengthOf(4)
+            expect(rotatedShape90 && rotatedShape180 && rotatedShape270).to.have.lengthOf(4)
+            assert(rotatedShape90.every(row => row.length === 4), 'not a 4x4 square')
+            assert(rotatedShape90.every(row => row.every(pt => typeof pt === 'number')), 'not all ints')
+        }
+        done()
+    })
+    it(`shape is identical to start shape when rotated 4 times`, done => {
+        for (let piece of TETROS.PIECES) {
+            const shape = pieceToShape(piece)
+            const rotatedShape360 = rotate(rotate(rotate(rotate(shape))))
+            expect(rotatedShape360).to.have.all.deep.members(shape)
+        }
+        done()
+    })
+    it('never goes outside its bounds when rotated', done => {
+        for (let piece of TETROS.PIECES) {
+            const maxXorY = _.max(_.flatten(piece))
+            const shape = pieceToShape(piece)
+
+            const rotations = [
+                rotate(shape),
+                rotate(rotate(shape)),
+                rotate(rotate(rotate(shape))),
+                rotate(rotate(rotate(rotate(shape)))) ]
+
+
+            rotations.forEach((rotation, i) => {
+                const toPiece = shapeToPiece(rotation)
+                assert(_.flatten(toPiece).every(val => val <= maxXorY), `when rotated ${90 * i}, [${piece}] has an X or Y > ${maxXorY}`)
+            })
+        }
+        done()
+    })
+})
 
 
 
